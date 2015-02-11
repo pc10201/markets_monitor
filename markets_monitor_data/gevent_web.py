d00a649c46dc21f52a9e4e213797390bb7088c9b
Liu Yang <pcliuyang@gmail.com>
2015-02-11 16:34:10 +0800

    清空不用的

--- markets_monitor_data/gevent_web.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Created  on 2014/9/12
-
-from gevent.wsgi import WSGIServer
-from monitor import app
-http_server = WSGIServer(('', 8000), app)
-http_server.serve_forever()
--- markets_monitor_data/settings_default.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-MYSQL_HOST = ''
-MYSQL_DBNAME = ''
-MYSQL_USER = ''
-MYSQL_PASSWD = ''
-MYSQL_PORT = 3306
-
-diff_seconds = 300
